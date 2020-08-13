93301f9caf682c12d74779ae122651ed64ec3e09
Juan Sebastián Suaza Solano <SsuazaA@gmail.com>
2020-08-12 19:31:49 -0500

    SecurityConfig basado en profiles

--- src/main/java/com/nisumlatam/userapp/config/SecurityConfigTest.java
+++ src/main/java/com/nisumlatam/userapp/config/SecurityConfigTest.java
@@ -2,27 +2,20 @@ package com.nisumlatam.userapp.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import com.nisumlatam.userapp.config.filter.JWTAuthorizationFilter;
 
 @Configuration
 @EnableWebSecurity
-@Profile(value = {"prod"})
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+@Profile(value = {"test"})
+public class SecurityConfigTest extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable()
-			.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
 			.authorizeRequests()
-			.antMatchers(HttpMethod.POST, "/api/login").permitAll()
-			.anyRequest().authenticated();
+			.anyRequest().permitAll();
 	}
 
 }
